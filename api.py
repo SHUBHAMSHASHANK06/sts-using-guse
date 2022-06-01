dc00a32f091d49b51666b9445b99e3b69da9eaa0
SHUBHAM SHASHANK <79024959+SHUBHAMSHASHANK06@users.noreply.github.com>
2022-06-01 20:15:37 +0530

    Update api.py

--- api.py
+++ api.py
@@ -9,4 +9,4 @@ async def input(text1 : str = Form(), text2 : str = Form()):
     return {"similarity_score": score(text1, text2)}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='127.0.0.1', port=4000, debug=True)
+    uvicorn.run(app, host='127.0.0.1', port=8000, debug=True)
