b0874986ecc7045caf99c768f9f4580b8fb88d42
SHUBHAM SHASHANK <79024959+SHUBHAMSHASHANK06@users.noreply.github.com>
2022-06-01 20:50:53 +0530

    Update api.py

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.post("/input")
 async def input(text1 : str = Form(), text2 : str = Form()):
-    return {"similarity_score": score(text1, text2)}
+    return ("similarity_score = ", score(text1, text2))
 
 if __name__ == "__main__":
     uvicorn.run(app, host='127.0.0.1', port=8000, debug=True)
